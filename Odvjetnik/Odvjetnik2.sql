6b6261f6e13c8926f8fe21f4b5b569931f8ed3a2
tjuros01 <49923586+tjuros01@users.noreply.github.com>
2019-05-28 13:34:06 +0200

    Naredbe insert i select

--- Odvjetnik/Odvjetnik2.sql
+++ Odvjetnik/Odvjetnik2.sql
@@ -50,3 +50,33 @@ alter table obrana add foreign key (klijent) references klijent(sifra);
 
 alter table suradnikobrana add foreign key (obrana) references obrana(sifra);
 alter table suradnikobrana add foreign key (suradnik) references suradnik(sifra);
+
+
+
+
+# NAREDBA INSERT
+
+
+insert into odvjetnik (ime,prezime,polog) values ('Ivan','Ivanković',1000.00),('Marko','Marković',899.00),('Davor','Dominiković',1200.00);
+
+insert into klijent (ime, prezime) values ('Marko','Radonić'),('Ivan','Ninković'),('Šimun','Lakušić');
+
+insert into suradnik (ime,prezime,zvanje,polog) values ('Dino','Čar','upravni referent',500.00),('Josip','Čurić','odvjetnik',450.00),
+('Zvonimir','Dmjehal','pomoćnik odvjetnika',650.00),
+('Mirko','Marko','referent',400.00);
+
+insert into obrana(odvjetnik,klijent) values (1,1),(2,3),(3,3),(2,2);
+insert into suradnikobrana(suradnik,obrana) values (1,1), (2,2), (3,3), (4,4);
+
+
+# NAREDBE SELECT I DELETE - VJEŽBE
+
+select * from odvjetnik;
+update odvjetnik set prezime='Orešković' where sifra=3;
+alter table odvjetnik add column iban varchar(32);
+update odvjetnik set iban='HR7523400096573698967' where sifra=2;
+insert into odvjetnik (ime,prezime,polog) values ('Ivo','Sanader','629.00');
+delete from odvjetnik where sifra=4;
+select * from odvjetnik;
+
+select 'Gotovo';
