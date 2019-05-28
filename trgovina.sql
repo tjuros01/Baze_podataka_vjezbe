1d1c8a758e4b7029da668e59fffd4f390c5d555e
tjuros01 <49923586+tjuros01@users.noreply.github.com>
2019-05-28 14:16:16 +0200

    Dodavanje vrijednosti

--- trgovina.sql
+++ trgovina.sql
@@ -1,3 +1,11 @@
+# izvođenje
+# Ova datoteka se nalazi na X:\
+/*
+
+c:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < "X:\trgovina.sql"
+
+*/
+
 drop database if exists trgovina;
 create database trgovina;
 use trgovina;
@@ -16,8 +24,12 @@ cijena decimal(18,2)
 create table stavka(
 racun int not null,
 proizvod int not null,
-kolicina decimal(18,3) not null
+kolicina decimal(18,2) not null
 );
 
 alter table stavka add foreign key (racun) references racun(sifra);
 alter table stavka add foreign key (proizvod) references proizvod(sifra);
+
+insert into proizvod (naziv,cijena) values ('monitor',899.00), ('tablet',1499.00), ('slušalice',99.00);
+insert into racun (datum) values ('2019-05-05');
+insert into stavka (racun, proizvod) values (1,1);
